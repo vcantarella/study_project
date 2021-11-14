734c8bce14eaeecd86b124811cd09ee58c0fa47c
vcantarella <vcantarella@gmail.com>
2021-11-14 19:37:42 +0100

    Model Proposal working code and preliminar test file

--- model_proposal.py
+++ model_proposal.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Nov 12 10:18:14 2021
 
-@author: vcant
+@authors: vcantarella, marigomez, alevilla
 """
 
 import numpy as np
@@ -37,7 +37,24 @@ class River:
 
 
 class Model:
-    def __init__(self, k, H, h0, river = None, x_ref = 0, y_ref = 0):
+    """
+    Basic AEM-RBF model Object. Inputs are the aquifer parameters and the river object
+    This object contains the methods to calculate discharge potential, heads and 
+    stream function from the aem objects.
+    
+    Currently only supports models with default river location (y-axis)
+    
+    Parameters
+    --------------------
+    k : hydraulic conductivity
+    H : aquifer Height
+    h0 : aquifer head at the reference location (measured from the base of the aquifer)
+    river: river object (not implemented yet)
+    x_ref : x-coordinate of the reference location
+    y_ref : y-coordinate of the reference location
+    
+    """
+    def __init__(self, k, H, h0, river = None, x_ref = 500, y_ref = 500):
         self.k = k
         self.H = H
         self.h0 = h0
@@ -46,7 +63,8 @@ class Model:
                                      'Discharge': [],
                                      'X': [],
                                      'Y': []})
-        if river == None:
+        self.Qo_x = 1e-2
+        if river is None:
             # If undeclared river line equation will be assumed to be located at the y-axis
             self.river_a = 1
             self.river_b = 0
@@ -63,6 +81,8 @@ class Model:
     def calc_phi(self, x, y):
         """
         Method to calculate the discharge potential at given location:
+        
+        Currently only supports models with default river location (y-axis)
 
         Parameters
         ----------
@@ -77,23 +97,79 @@ class Model:
         phi_well = 0
         for element in self.aem_elements:
             d = np.abs(self.river_a *element.x + self.river_b*element.y + self.river_c)/np.sqrt(self.river_a**2+ self.river_b**2)
-            phi_q = (element.Q/(4*np.pi))*np.log(((x + d)**2 + y**2)/((x-d)**2+y**2))
+            if (x == element.x):
+                phi_q = phi_q = (element.Q/(2*np.pi))*np.log((np.sqrt(x+element.rw -element.x)**2 + (y-element.y)**2)/np.sqrt((x+element.rw-(element.x-2*d))**2+(y-element.y)**2))
+            elif (y == element.y):
+                phi_q = phi_q = (element.Q/(2*np.pi))*np.log(np.sqrt((x - element.x)**2 + (y+element.rw-element.y)**2)/np.sqrt((x-(element.x - 2*d))**2+(y+element.rw-element.y)**2))
+            else:
+                phi_q = (element.Q/(2*np.pi))*np.log(np.sqrt((x - element.x)**2 + (y-element.y)**2)/np.sqrt((x-(element.x - 2*d))**2+(y-element.y)**2))
             phi_well += phi_q
-        return self.phi0 + phi_well
-    def head(self,x,y):
-        phi = self.calc_phi(x,y)
-        phicrit = self.phi0 + 0.5 * self.k * self.H **2 
-        if phi >= phicrit:
-            h = 0.5 * self.H + (1 / self.k / self.H) *phi
-        else:
+        phi_base = self.Qo_x*x
+        return self.phi0 + phi_well + phi_base
+    
+    def calc_head(self,x,y):
+        """
+        Method to calculate the head at a given location:
+        
+        Currently only supports models with default river location (y-axis)
+
+        Parameters
+        ----------
+        x,y location of interest.
+
+        Returns
+        -------
+        head at (x,y): float.
+
+        """
+        phi = self.calc_phi(x,y) # Discharge potential
+        phicrit = 0.5 * self.k * self.H **2 #Method according to Haijtema, 1995
+        if phi >= phicrit: # Confined conditions
+            h = (phi + 0.5*self.k*self.H**2)/(self.k*self.H)
+        else: # Unconfined conditions
             h = np.sqrt((2 / self.k) * (phi)) 
         return h# head
-            
     
+    def calc_psi(self, x,y):
+        """
+        Method to calculate the stream function for the RBF-AEM model at 
+        a given location (x,y)
+        
+        Currently only supports models with default river location (y-axis)
+
+        Parameters
+        ----------
+        x,y: coordinates of location of interest.
+
+        Returns
+        -------
+        Stream function (psi) at (x,y): float.
+
+        """
+        psi_well = 0
+        for element in self.aem_elements:
+            d = np.abs(self.river_a *element.x + self.river_b*element.y + self.river_c)/np.sqrt(self.river_a**2+ self.river_b**2)
+            if (x == element.x) & (y == element.y):
+                psi_q = (element.Q/(2*np.pi))*(np.arctan2((y-element.y),(x-(element.x+element.rw)))- np.arctan2((y-element.y),(x-(element.x-2*d))))
+            else:
+                psi_q = (element.Q/(2*np.pi))*(np.arctan2((y-element.y),(x-element.x))- np.arctan2((y-element.y),(x-(element.x-2*d))))
+            psi_well += psi_q
+        psi_base = self.Qo_x*y
+        psi = psi_well+psi_base
+        
+        
+        return psi
 
 class Well:
     """
-    Element to create Well for the model
+    Element to create Well for the AEM-RBF model
+    Inputs to the well object:
+    --------------------------
+        model : a AEM-RBF model
+        Q: discharge rate
+        rw: radius of the well
+        x: x-location of the well
+        y: y-location of the well
     """
     def __init__(self, model,Q, rw, x,y):
         self.x = x
@@ -101,8 +177,6 @@ class Well:
         self.Q = Q
         self.rw = rw
         model.aem_elements.append(self)
-        def phi(self):
-            self.Q/2*np.pi
 
 
 
@@ -111,7 +185,7 @@ aem_model = Model(k = 10, H = 20, h0 = 18)
 well = Well(aem_model, Q = 10, rw = 0.2, x = 30, y = 50)
 
 
-print(aem_model.head(1, 1))
+print(aem_model.calc_psi(225, 0))
 
 
 
--- test.py
+++ test.py
@@ -12,39 +12,54 @@ import matplotlib.pyplot as plt
 
 
 
-aem_model = model_proposal.Model(k = 10, H = 20, h0 = 18)
-
-well = model_proposal.Well(aem_model, Q = 10, rw = 0.2, x = 30, y = 50)
+aem_model = model_proposal.Model(k = 3, H = 20, h0 = 18)
 
 
-xvec = np.linspace(0, 300, 100)
-yvec = np.linspace(0, 300, 100)
+well = model_proposal.Well(aem_model, Q = 200, rw = 0.2, x = 30, y = 50)
+
+
+xvec = np.linspace(0, 150, 100)
+yvec = np.linspace(-100, 100, 100)
+
+xvec, yvec = np.meshgrid(xvec,yvec)
 
 
 h = []
-for x,y in zip(xvec,yvec):
-    head = aem_model.head(x, y)
-    h.append(h)
-    
+psi = []
+for x,y in zip(xvec.flatten(),yvec.flatten()):
+    print(x)
+    print(y)
+    head = aem_model.calc_head(x, y)
+    psi_0 = aem_model.calc_psi(x, y)
+    print(head)
+    h.append(head)
+    psi.append(psi_0)
+print(h)
+h = np.array(h).reshape((100,100))
+psi = np.array(psi).reshape((100,100))
 from matplotlib import cm
-fig2, ax = plt.subplots()
-contour = plt.contour(xvec, yvec, h,
-    5,
+fig2, ax = plt.subplots(figsize = (15,20))
+contour = plt.contourf(xvec, yvec, h,
+    8,
     cmap = cm.Blues)
     #colors=['#808080', '#A0A0A0', '#C0C0C0'], extend='both')
 ax.set_xlabel('x [m]')
 ax.set_ylabel('y [m]')
+fig2.colorbar(contour, ax=ax, shrink=0.9)
+contour_psi = plt.contour(xvec,yvec,psi,
+                          10,
+                          colors=('darkgrey',),
+                          linewidths=(2,))
+
+#ax.clabel(contour_psi, fmt='%2.1f', colors='w', fontsize=14)
 # fig2, ax = plt.subplots()
 #contour2 = plt.contour(xvec ,yvec, psi,
     #gstream,
+
     #cmap = cm.binary
     #colors=['#808080', '#808080', '#808080'], extend='both')
-contour.cmap.set_over('red')
-contour.cmap.set_under('blue')
-contour.changed()
 #plt.clabel(contour, inline=1, fontsize=10)
-labels = ['Streamline', 'Potentialline']
+#labels = ['Streamline', 'Potentialline']
 #contour2.collections[6].set_label(labels[0])
-contour.collections[7].set_label(labels[1])
+#contour.collections[7].set_label(labels[1])
 
-plt.legend(loc='upper left')
