215b1772a601159a8303ffcc39315b0308cf8e0a
vcantarella <vcantarella@gmail.com>
2022-01-12 21:47:08 +0100

    Time of travel implementation

--- solvers.py
+++ solvers.py
@@ -18,7 +18,8 @@ Limitations:
 """
 import numpy as np
 import sympy
-
+import itertools
+from scipy.optimize import fsolve
 
 class river_length():
     """
@@ -74,7 +75,7 @@ class river_length():
             ex_st_points = Q/(np.pi*d*Qx)
             
             if ex_st_points <= 1:
-                return print(" There are no stagnation points, check model inputs")
+                return print("There are no stagnation points, check model inputs")
             else:
                 equation = sympy.Eq(-d**2 + d*Q/(np.pi*Qx)-y**2,0) # Equation assumes the well is at y = 0, it is corrected later below.
                 sols = sympy.solveset(equation, y, domain = sympy.S.Reals)
@@ -86,6 +87,130 @@ class river_length():
                 contrib = Q_river/Q
             
                 return length,sol_el, contrib
+    def time_travel(self, ne, delta_s = 0.1):
+        """
+        Method to derive the time of travel of selected paths from the river to the well
+        
+        
+
+        Returns
+        -------
+        time of travel array : numpy array, dimensions: (length of river capture length,1)
+
+        """
+        
+        length, sol_el, contrib = self.solve_river_length()
+        
+        ys = np.linspace(sol_el[0]+0.5,sol_el[1]-0.5,int(sol_el[1]-sol_el[0]-1))
+        print(ys.shape)
+        xs = np.repeat(0.1,ys.shape[0])
+        tt = []
+        
+        '''
+        initial parameters from the model:
+            
+        '''
+        elem = self.model.aem_elements[0]
+        Q = elem.Q
+        xw = elem.x
+        yw = elem.y
+        d = np.abs(self.model.river_a *xw + self.model.river_b*yw + self.model.river_c)/np.sqrt(self.model.river_a**2+ self.model.river_b**2)
+        Qx = self.model.Qo_x
+        rw = elem.rw
+        
+        '''
+        general qx, qy formulas (general potential)
+        '''
+        def qx(x,y):
+            return -1*(-Qx + Q/(4*np.pi)*((2*(x-xw)/((x-xw)**2+(y-yw)**2))-(2*(x-(xw-2*d)))/((x-(xw-2*d))**2+(y-yw)**2)))/self.model.calc_head(x, y)
+        
+        def qy(x,y):
+            return -1*(Q/(4*np.pi)*((2*(y-yw)/((x-xw)**2+(y-yw)**2))-(2*(y-yw))/((x-(xw-2*d))**2+(y-yw)**2)))/self.model.calc_head(x, y)
+        
+        ''' 
+        calculation of streamline and time of travel
+        '''
+        for x,y in zip(xs,ys):
+            
+            dis_arr = []
+            v_arr = []
+            
+            x1 = x
+            y1 = y
+            while np.sqrt((x1-xw)**2+(y1-yw)**2) > 1.5*delta_s :
+                #Part 1 calculating velocity:
+                dista1 = np.sqrt((x1-xw)**2+(y1-yw)**2)
+                print(x1)
+                print(y1)
+                qx1 = qx(x1,y1)
+                qy1 = qy(x1,y1)
+                vx = qx1/ne
+                vy = qy1/ne
+                v_i = np.sqrt(vx**2+vy**2)
+                v_arr.append(v_i)
+                
+                #Part 2: estimating second point
+                
+                x_2 = np.float(x1 + delta_s*vx/v_i)
+                y_2 = np.float(y1 + delta_s*vy/v_i)
+                
+                psi = self.model.calc_psi(x,y)
+                
+                
+                ## Adjust for psi:
+                #x_a = sympy.symbols('x_a')
+                #y_a = sympy.symbols('y_a')
+                def equation_x(x_a):
+                    return Qx*y_2 + (Q/(2*np.pi))*(np.arctan2((y_2-yw),(x_a-xw))- np.arctan2((y_2-yw),(x_a-(xw-2*d))))-psi
+                sols = fsolve(equation_x, x_2)
+                sol_el_x = sols[0]
+                
+                def equation_y(y_a) :
+                    return Qx*y_a + (Q/(2*np.pi))*(np.arctan2((y_a-yw),(x_2-xw))- np.arctan2((y_a-yw),(x_2-(xw-2*d))))-psi
+                sols_y = fsolve(equation_y, y_2)
+                sol_el_y = sols_y[0]
+                
+                pos_locs_y = [(sol_el_x,y_2)]
+                pos_locs_x = [(x_2,sol_el_y)]
+                pos_locs = pos_locs_x + pos_locs_y
+
+                dista = 1e9
+                print(pos_locs)
+                #Calculating distance and correcting second point:
+                for xp,yp in pos_locs:
+                    dist = np.sqrt((xp-x1)**2+(yp-y1)**2)
+                    if dist < dista:
+                        x_2 = xp
+                        y_2 = yp
+                        dista = dist
+                dis_arr.append(dista)
+                print('distancia da particula:')
+                print(np.sqrt((x1-xw)**2+(y1-yw)**2))
+                #Looping
+                x1 = x_2
+                y1 = y_2
+                dista_2 = np.sqrt((x1-xw)**2+(y1-yw)**2)
+                if dista_2 > dista1:
+                    break
+            #Adding time of travel estimate
+            dis_arr = np.array(dis_arr)
+            v_arr = np.array(v_arr)
+            tt.append(np.sum(dis_arr/v_arr))
+            print("Essa particula CHEGOU!!!")
+            
+        return tt
+            
+            
+            
+                
+            
+            
+            
+            
+            
+            
+            
+            
         
         
 
--- test2.py
+++ test2.py
@@ -10,7 +10,8 @@ import solvers
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+### Example 01: One well: River capture length, percentage of discharge
+#-------------------------------------------
 
 aem_model = model_proposal.Model(k = 3, H = 20, h0 = 18)
 
@@ -64,5 +65,48 @@ contour_psi_river = plt.contour(xvec,yvec,psi,
                           colors=('darkred',),
                           linewidths=(7,))
 
+river_line = plt.plot([0,0],[np.min(yvec),np.max(yvec)], color = 'blue', linestyle = '-', linewidth = 16)
 river_capture = plt.plot([0, 0], [riv_coords[0], riv_coords[1]], color='r', linestyle='-', linewidth=8)
 
+tt = solv.time_travel(0.2)
+print("Time of Travel: ")
+print(tt)
+### Example 0: Multiple Wells:
+#-------------------------------------------
+
+well3 = model_proposal.Well(aem_model, Q = 150, rw = 0.2, x = 10, y = 30)
+
+well4 = model_proposal.Well(aem_model, Q = 50, rw = 0.2, x = 50, y = 10)
+
+xvec = np.linspace(0, 60, 100)
+yvec = np.linspace(00, 60, 100)
+
+xvec, yvec = np.meshgrid(xvec,yvec)
+
+h = []
+psi = []
+for x,y in zip(xvec.flatten(),yvec.flatten()):
+    
+    head = aem_model.calc_head(x, y)
+    psi_0 = aem_model.calc_psi(x, y)
+    
+    h.append(head)
+    psi.append(psi_0)
+
+h = np.array(h).reshape((100,100))
+psi = np.array(psi).reshape((100,100))
+from matplotlib import cm
+fig2, ax = plt.subplots(figsize = (15,15))
+contour = plt.contourf(xvec, yvec, h,
+    8,
+    cmap = cm.Blues, alpha = 0.7)
+    #colors=['#808080', '#A0A0A0', '#C0C0C0'], extend='both')
+ax.set_xlabel('x [m]')
+ax.set_ylabel('y [m]')
+fig2.colorbar(contour, ax=ax, shrink=0.9)
+contour_psi = plt.contour(xvec,yvec,psi,
+                          20,
+                          colors=('darkgrey',),
+                          linewidths=(4,),
+                          linestyle = ('-',))
+river_line = plt.plot([0,0],[np.min(yvec),np.max(yvec)], color = 'blue', linestyle = '-', linewidth = 16)
